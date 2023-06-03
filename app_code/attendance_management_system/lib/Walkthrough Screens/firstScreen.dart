adbc117922af4c3f9a8efa46af20f7fd9b652ed6
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-06-03 23:33:34 +0530

    Update: Updates to Walkthrough screens

--- app_code/attendance_management_system/lib/Walkthrough Screens/firstScreen.dart
+++ app_code/attendance_management_system/lib/Walkthrough Screens/firstScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 
 class firstScreen extends StatefulWidget {
   const firstScreen({super.key});
@@ -27,56 +26,59 @@ class _firstScreenState extends State<firstScreen>
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Center(
-              child: Column(
-                children: [
-                  Padding(
-                    padding:
-                        const EdgeInsets.only(top: 80, left: 45, right: 45),
-                    child: Container(
-                        decoration: BoxDecoration(),
-                        child: Lottie.asset('assets/animations/Main.json')
-                        // Lottie.network(
-                        //   'https://assets10.lottiefiles.com/packages/lf20_3rqwsqnj.json',
-                        // controller: _animationController,
-                        // animate: true,
-                        // ),
-                        ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: const TextSpan(
-                      children: [
-                        TextSpan(
-                            text:
-                                "Check Attendance. Calculate missed\nclasses. See nearing holidays.\n",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        // TextSpan(
-                        //     text: "See nearing holidays. \n",
-                        //     style: TextStyle(color: Colors.black)),
-                        TextSpan(
-                            text: "All of it in One place.",
-                            style: TextStyle(
-                              color: Colors.black,
-                              fontSize: 17,
-                              fontWeight: FontWeight.bold,
-                            )),
-                      ],
-                    ),
-                  ),
-                ],
+        child: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70, left: 30, right: 30),
+                child: Image.asset(
+                  'assets/images/1finall.gif',
+                  // scale: 0,
+                ),
               ),
-            ),
-          ],
+              // Padding(
+              //   padding:
+              //       const EdgeInsets.only(top: 80, left: 45, right: 45),
+              //   child: Container(
+              //       decoration: BoxDecoration(),
+              //       child: Lottie.asset('assets/animations/Main.json')
+              //       // Lottie.network(
+              //       //   'https://assets10.lottiefiles.com/packages/lf20_3rqwsqnj.json',
+              //       // controller: _animationController,
+              //       // animate: true,
+              //       // ),
+              //       ),
+              // ),
+              const SizedBox(
+                height: 40,
+              ),
+              RichText(
+                textAlign: TextAlign.center,
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                        text:
+                            "Check Attendance. Calculate missed\nclasses. See nearing holidays.\n",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        )),
+                    // TextSpan(
+                    //     text: "See nearing holidays. \n",
+                    //     style: TextStyle(color: Colors.black)),
+                    TextSpan(
+                        text: "All of it in One place.",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 17,
+                          fontWeight: FontWeight.bold,
+                        )),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

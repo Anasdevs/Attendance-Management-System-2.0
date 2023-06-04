b7f780641d5f5c82f761d98e7e3821ff855c2d44
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-06-05 00:36:55 +0530

    db almost done

--- backend/ams/models/admin.py
+++ backend/ams/models/admin.py
@@ -1,46 +1,53 @@
-import Querries
+from Queries import *
 
 class College:
     def __init__(self, college_name):
         self.name = college_name
-        # self.id = college_id
-        # self.city = college_city
-        # self.state = college_state
-        # self.country = college_country
-        # self.zip = college_zip
-        # self.phone = college_phone
-        # self.email = college_email
-        # self.website = college_website
-        # self.description = college_description
-        # self.address = college_address
-        self.students_class = {}
-        self.courses = {}
+        self.classes = {}
         self.instructors = {}
-    
+
+    # ___________ teachers _____________________
     def add_new_class(self, cource_name:str, sem:int, section:str, sub:str, teacher_assigned:int, students:dict):
         new_class = Class(cource_name,sem,section, sub, teacher_assigned, students)
         cource_id = (cource_name.replace(' ', '_')+"-"+sub+"-"+str(sem)+"-"+section)
-        if cource_id in self.students_class.keys():
+        if cource_id in self.classes.keys():
             cource_id+="#"
-        self.students_class[cource_id] = new_class
+        new_class.setClassID(cource_id)
+        save_class_to_mongodb(class_name=cource_id, class_obj=new_class)
+        self.classes[cource_id] = new_class
         return ["succsss", cource_id]
 
     def get_all_class(self):
-        return self.students_class.keys()
-    
+        return self.classes.keys()
+
+    def loadCourses(self):
+        for i in self.classes.keys():
+            Cource = get_class_from_mongodb(i)
+            self.classes[i] = Cource
+        return True
+
+    # __________ teachers ____________
     def add_new_teacher(self, username,email,password,
                         name:str = "__blank__", title:str="__blank__", special_subject:str = "__blank__"):
         if username in self.instructors.keys():
             return [False, "already exist"]
         new_teacher = Teacher(username, email, password, name, title, special_subject)
-        teacher_id = username
-        self.instructors[teacher_id] = new_teacher
-        return ["succsss", teacher_id]
+        save_teacher_to_mongodb(new_teacher)        
+        self.instructors[username] = new_teacher
+        return ["succsss", username]
 
+    def load_teachers(self):
+        for i in self.instructors.keys():
+            teacher = load_teacher_from_mongodb(i)
+            self.instructors[i] = teacher
+        return True
+
+
+    # ___________ WHOLE SERVER ____________
     def save(self):
-        result = Querries.save_college_to_mongodb(self)
-        return Querries.write_key_to_json(key=self.name, value=result)
-    
+        result = save_college_to_mongodb(self)
+        return write_key_to_json(key=self.name, value=result)
+
 class Teacher:
     def __init__(self, username, email, password, name, title, special_subject):
         self.username = username
@@ -49,7 +56,7 @@ class Teacher:
         self.name = name
         self.title = title
         self.special_subject = special_subject
-        self.students = {}
+        self.Courses = []
         
     def login(self, username, password):
         if username == self.username and password == self.password:
@@ -60,16 +67,21 @@ class Teacher:
 
 class Class:
     def __init__(self, cource_name, sem, section, sub, teacher_assigned, students):
+        self.classId=""
         self.cource_name = cource_name
         self.sem = sem
         self.section = section
         self.sub = sub
         self.teacher_assigned = teacher_assigned
         self.students = students
-    
+
+    def setClassID(self,id):
+        self.classId = id
+
     def take_Attendence(self):
         # make object {student_name: "present"/"absent"} and feed it to mongodb database with today's date
         pass
+
     def __str__(self) -> str:
         return (self.cource_name.replace(' ', '_')+"-"+self.sub+"-"+str(self.sem)+"-"+self.section)
 

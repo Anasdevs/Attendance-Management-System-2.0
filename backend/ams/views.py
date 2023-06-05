df6b8eb522fdd750114bf195c9774da4b6bcc0c6
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-06-06 01:26:57 +0530

    api working abit

--- backend/ams/views.py
+++ backend/ams/views.py
@@ -1,56 +1,45 @@
-# from django.contrib.auth import authenticate, login, logout
-# from django.contrib.auth.models import User
-# from django.http import JsonResponse
-# import random
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+import random
 
-# def home(request):
-#     data = {
-#         'message': 'Hello, World!'
-#     }
-#     return JsonResponse(data)
+def home(request):
+    data = {
+        'message': 'Hello, World!'
+    }
+    return JsonResponse(data)
 
 # import json
 # from django.conf import settings
 # from django.core.mail import send_mail
-# from django.http import JsonResponse
 
-# def sendMail(request):
-#     if request.method == 'POST':
-#         try:
-#             data = json.loads(request.body)
-#             name = data.get('name')
-#             email = data.get('email')
-#             message = data.get('message')
-#             to = data.get('to')
-            
-#             if name and email and message and to:
-#                 send_mail(
-#                     'Contact Form Submission',
-#                     'Name: {0} \nEmail: {1} \nMessage: {2}'.format(name, email, message),
-#                     settings.DEFAULT_FROM_EMAIL,
-#                     list(to)
-#                 )
-#                 return JsonResponse({'status': 'success'})
-#             else:
-#                 return JsonResponse({'status': 'error', 'message': 'Missing required fields'})
-#         except json.JSONDecodeError:
-#             return JsonResponse({'status': 'error', 'message': 'Invalid JSON payload'})
-#     return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
-
-<<<<<<< Updated upstream
-def signup(request):
+def sendMail(request):
     if request.method == 'POST':
-        email = request.POST.get('email')
-        # password = request.POST.get('password')
-=======
-
-
+        try:
+            data = json.loads(request.body)
+            name = data.get('name')
+            email = data.get('email')
+            message = data.get('message')
+            to = data.get('to')
+            
+            if name and email and message and to:
+                send_mail(
+                    'Contact Form Submission',
+                    'Name: {0} \nEmail: {1} \nMessage: {2}'.format(name, email, message),
+                    settings.DEFAULT_FROM_EMAIL,
+                    list(to)
+                )
+                return JsonResponse({'status': 'success'})
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Missing required fields'})
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON payload'})
+    return JsonResponse({'status': 'error', 'message': 'Invalid request method'})
 
 # def signup(request):
 #     if request.method == 'POST':
 #         email = request.POST.get('email')
 #         # password = request.POST.get('password')
->>>>>>> Stashed changes
 
 #         if User.objects.filter(email=email).exists():
 #             return JsonResponse({'error': 'Username is already taken'}, status=400)
@@ -85,20 +74,12 @@ def signup(request):
 #     logout(request)
 #     return JsonResponse({'message': 'Logout successful'})
 
-<<<<<<< Updated upstream
-from models import MSI
-from admin import *
-
-=======
-
-
-# from models import MSI
->>>>>>> Stashed changes
+from .models import MSI
 
 # def Dashboard(request):
 #     return 
     # get teacher's profile
-    teacherIDs = MSI.
+    # teacherIDs = MSI.
     # get class list
 
 
@@ -121,19 +102,22 @@ from admin import *
 # view for admin login
 from django.http import JsonResponse, HttpResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 import json
-
+@api_view(['POST'])
 def adminlogin(request):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        login_id = data.get('loginId')
-        password = data.get('password')
+    # if request.method == 'POST':
+    data = json.loads(request.body)
+    login_id = data.get('loginId')
+    password = data.get('password')
 
-        if login_id == 'anass' and password == 'anass':
-            # Return success response
-            return JsonResponse({'message': 'Login successful'}, status=200)
-        else:
-            # Return error response
-            return JsonResponse({'message': 'Invalid login credentials'}, status=400)
+    if login_id == 'anass' and password == 'anass':
+        # Return success response
+        return JsonResponse({'message': 'Login successful'}, status=200)
+    else:
+        # Return error response
+        return JsonResponse({'message': 'Invalid login credentials'}, status=400)
 
-    return JsonResponse({'message': 'Invalid request'}, status=400)
+    # return JsonResponse({'message': 'Invalid request'}, status=400)

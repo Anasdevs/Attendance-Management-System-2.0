2f542274b73ce8ef6c7b0ec21d62876547b6bf85
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-06-06 01:26:57 +0530

    api working abit

--- backend/ams/models/__init__.py
+++ backend/ams/models/__init__.py
@@ -1,7 +1,5 @@
-<<<<<<< Updated upstream
-import database
-from Queries import *
-from admin import *
+from .Queries import *
+from .admin import *
 college_id = read_key_from_json("MSI")
 if college_id == None:
     MSI = College(
@@ -20,24 +18,4 @@ else:
             college_name="MSI",
             )
         MSI.save()
-=======
-# import database
-# from Querries import *
-# from admin import *
-# college_id = read_key_from_json("MSI")
-# if college_id == None:
-#     MSI = College(
-#         college_name="MSI",
-#         )
-#     MSI.save()
 
-# else:
-#     try:
-#         MSI = retrieve_college_from_mongodb(college_id=college_id)
-#     except:
-#         print("unable to extract college detail, creating new ones")
-#         MSI = College(
-#             college_name="MSI",
-#             )
-#         MSI.save()
->>>>>>> Stashed changes

66f0c5de1a61545674b455a4ac90fcbd32effee2
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-06-09 23:05:03 +0530

    Update: Updates to HomeScreen

--- app_code/attendance_management_system/lib/main.dart
+++ app_code/attendance_management_system/lib/main.dart
@@ -20,14 +20,14 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // theme: ThemeData.light().copyWith(
-      //   textTheme: GoogleFonts.adventProTextTheme(
-      //     Theme.of(context).textTheme,
-      //   ),
-      // ),
+      theme: ThemeData.light().copyWith(
+        textTheme: GoogleFonts.adventProTextTheme(
+          Theme.of(context).textTheme,
+        ),
+      ),
       debugShowCheckedModeBanner: false,
       home: MaterialApp(
-        home: const demo(),
+        home: const walkthrough(),
         initialRoute: Routes.wts,
         routes: {
           // "/": (context) => loginScreen(),

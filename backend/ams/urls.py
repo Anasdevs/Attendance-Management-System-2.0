592b743e2968ac379b49c5c8bbed5cdf6c7324ee
Harsh Agnihotri <harshagnihotri90@gmail.com>
2023-06-10 19:42:09 +0530

    its working

--- backend/ams/urls.py
+++ backend/ams/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.test, name="test"),
     path('adminlogin', views.adminlogin, name='adminlogin'),
     # path('admin/teacher_signup/', csrf_exempt(views.teacher_register.as_view()), name='register'),
-    path('admin/teacher_signup/', views.teacher_register, name='register'),
+    path('teacher/signup', views.teacher_register, name='register'),
     path('teacher/login/', views.teacher_login, name='login'),
     path('teacher/dashboard', views.teacher_dashboard, name='login'),
     path('class/create_class',views.create_class, name="create_class"),
@@ -14,14 +14,3 @@ urlpatterns = [
     # path('get_month_attendence/', views.sendMail, name='send_mail'),
     # path('get_attendence_report/', views.sendMail, name='send_mail'),
 ]
-# urlpatterns = [
-#     path('', views.test, name="test"),
-#     url('adminlogin', views.adminlogin, name='adminlogin'),
-#     path('admin/teacher_signup/', csrf_exempt(views.teacher_register.as_view()), name='register'),
-#     path('teacher/login/', views.teacher_login, name='login'),
-#     path('teacher/dashboard', views.teacher_dashboard, name='login'),
-#     path('class/create_class',views.create_class, name="create_class"),
-#     # path('send_attendence/', views.sendMail, name='send_mail'),
-#     # path('get_month_attendence/', views.sendMail, name='send_mail'),
-#     # path('get_attendence_report/', views.sendMail, name='send_mail'),
-# ]

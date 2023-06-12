e3564c044fb3b2700f5b3f45db491cfa696c1b91
Amanjot Singh <amanjot02121202021@msijanakpuri.com>
2023-06-12 15:51:53 +0530

    Fix: Fixed Submit screen

--- app_code/attendance_management_system/lib/Screens/submit.dart
+++ app_code/attendance_management_system/lib/Screens/submit.dart
@@ -15,57 +15,62 @@ class _submitState extends State<submit> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 250, left: 130, right: 130),
-              child: Image.asset(
-                'assets/images/submit.gif',
-                // scale: 0,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 70,
-          ),
-          Text(
-            "Attendance Submitted Successfully!",
-            style: TextStyle(
-                color: btColor, fontSize: 22, fontFamily: "PoppinsSemi"),
-          ),
-          const SizedBox(
-            height: 200,
-          ),
-          SizedBox(
-            height: 50,
-            width: 100,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xFF5e66e0),
-                elevation: 7,
-              ),
-              onPressed: () {
-                Navigator.pushAndRemoveUntil(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => demo(),
-                    ),
-                    (route) => false);
-              },
-              child: const Text(
-                "Home",
-                style: TextStyle(
-                  // color: Color(0xFF5e66e0),
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  fontFamily: 'PoppinsSemi',
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 250, left: 130, right: 130),
+                  child: Image.asset(
+                    'assets/images/submit.gif',
+                    // scale: 0,
+                  ),
                 ),
               ),
-            ),
+              const SizedBox(
+                height: 70,
+              ),
+              Text(
+                "Attendance Submitted Successfully!",
+                style: TextStyle(
+                    color: btColor, fontSize: 22, fontFamily: "PoppinsSemi"),
+              ),
+              const SizedBox(
+                height: 200,
+              ),
+              SizedBox(
+                height: 50,
+                width: 100,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF5e66e0),
+                    elevation: 7,
+                  ),
+                  onPressed: () {
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => demo(),
+                        ),
+                        (route) => false);
+                  },
+                  child: const Text(
+                    "Home",
+                    style: TextStyle(
+                      // color: Color(0xFF5e66e0),
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      fontFamily: 'PoppinsSemi',
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }

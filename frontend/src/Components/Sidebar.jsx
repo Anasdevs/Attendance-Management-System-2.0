1e627e7c315e59a9949a71fdb61e90cf4d1e434a
Anasdevs <anassaifi1108@gmail.com>
2023-06-12 14:00:03 +0530

    Refactor frontend

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,33 +1,54 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartBar, faCalendarAlt, faCalendarCheck, faUser } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
+import msilogo from './Images/msilogo.png';
 
 export default function Sidebar() {
   return (
     <div>
       <div className="sidebar">
-        <h2 className="ams">AMS</h2>
+        <div className="sidebar-up">
+          <img src={msilogo} alt="College Logo" className="logo" />
+          <h2 className="ams">AMS</h2>
+        </div>
+        <hr />
         <ul>
-          {/* <li className="menu-heading">Main Menu</li> */}
           <li>
             <NavLink to="/dashboard" activeClassName="active">
-              <span className="icon">📊</span>
+              <span className="icon">
+                <FontAwesomeIcon icon={faChartBar} />
+              </span>
               Dashboard
             </NavLink>
           </li>
           <li>
             <NavLink to="/holidays" activeClassName="active">
-              <span className="icon">📅</span>
+              <span className="icon">
+                <FontAwesomeIcon icon={faCalendarAlt} />
+              </span>
               Holidays
             </NavLink>
           </li>
           <li>
             <NavLink to="/calendar" activeClassName="active">
-              <span className="icon">🗓️</span>
+              <span className="icon">
+                <FontAwesomeIcon icon={faCalendarCheck} />
+              </span>
               Calendar
             </NavLink>
           </li>
+          <li><NavLink to="/profile" activeClassName="active">
+            <span className="icon">
+              <FontAwesomeIcon icon={faUser} />
+            </span>
+            Profile
+          </NavLink></li>
         </ul>
+        <div className="profile-button">
+          
+        </div>
       </div>
     </div>
   );

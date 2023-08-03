c94b9e39aa9b8fa1a63b1c53db611d3211002aa7
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-03 13:13:59 +0530

    Sidebar and dashboard Responsive

--- frontend/src/Components/Sidebar.jsx
+++ frontend/src/Components/Sidebar.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChartBar, faCalendarAlt, faCalendarCheck, faUser, faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faChartBar, faCalendarAlt, faCalendarCheck, faUser, faFileAlt, faBars } from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 import msilogo from './Images/msilogo.png';
 
 export default function Sidebar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar((prevShowSidebar) => !prevShowSidebar);
+  };
+
   return (
     <div>
-      <div className="sidebar">
+      <div className={`sidebar ${showSidebar ? 'show' : ''}`}>
         <div className="sidebar-up">
           <img src={msilogo} alt="College Logo" className="logo" />
           <h2 className="ams">AMS</h2>
@@ -16,7 +22,7 @@ export default function Sidebar() {
         <hr />
         <ul>
           <li>
-            <NavLink to="/dashboard" activeclassname="active">
+            <NavLink to="/dashboard" activeClassName="active">
               <span className="icon">
                 <FontAwesomeIcon icon={faChartBar} />
               </span>
@@ -24,7 +30,7 @@ export default function Sidebar() {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/calendar" activeclassname="active">
+            <NavLink to="/calendar" activeClassName="active">
               <span className="icon">
                 <FontAwesomeIcon icon={faCalendarCheck} />
               </span>
@@ -32,21 +38,25 @@ export default function Sidebar() {
             </NavLink>
           </li>
           <li>
-            <NavLink to="/reports" activeclassname="active">
+            <NavLink to="/reports" activeClassName="active">
               <span className="icon">
                 <FontAwesomeIcon icon={faFileAlt} />
               </span>
               Reports
             </NavLink>
           </li>
-          <li><NavLink to="/profile" activeclassname="active">
-            <span className="icon">
-              <FontAwesomeIcon icon={faUser} />
-            </span>
-            Profile
-          </NavLink></li>
+          <li>
+            <NavLink to="/profile" activeClassName="active">
+              <span className="icon">
+                <FontAwesomeIcon icon={faUser} />
+              </span>
+              Profile
+            </NavLink>
+          </li>
         </ul>
-        <div className="profile-button">
+        <div className="profile-button"></div>
+        <div className="hamburger" onClick={toggleSidebar}>
+          <FontAwesomeIcon icon={faBars} />
         </div>
       </div>
     </div>

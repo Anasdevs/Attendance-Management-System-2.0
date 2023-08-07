49e7c30801c027234a3d2f80720a9bb85590c42d
Anasdevs <anassaifi1108@gmail.com>
2023-08-07 17:40:00 +0530

    Modularizing models

--- backend/ams/admin.py
+++ backend/ams/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Faculty, Class, Bca_Student, Bca_Attendance, Bba_Student, Bba_Attendance, B_Com_Student,B_com_Attendance,B_Ed_Student,B_Ed_Attendance,Mba_Student,Mba_Attendance,Law_Student,Law_Attendance
+from .models import Faculty, Class, Bca_Student, Bca_Attendance, Bba_Student, Bba_Attendance, B_Com_Student, B_Com_Attendance,B_Ed_Student,B_Ed_Attendance,Mba_Student,Mba_Attendance,Law_Student,Law_Attendance
 
 admin.site.register(Faculty)
 admin.site.register(Class)
@@ -8,7 +8,7 @@ admin.site.register(Bca_Attendance)
 admin.site.register(Bba_Student)
 admin.site.register(Bba_Attendance)
 admin.site.register(B_Com_Student)
-admin.site.register(B_com_Attendance)
+admin.site.register(B_Com_Attendance)
 admin.site.register(B_Ed_Student)
 admin.site.register(B_Ed_Attendance)
 admin.site.register(Mba_Student)

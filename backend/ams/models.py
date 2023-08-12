eb2a9d6b290bfe031d13de3570a7c97dd332373b
Anasdevs <anassaifi1108@gmail.com>
2023-08-12 15:47:18 +0530

    Report for HOD

--- backend/ams/models.py
+++ backend/ams/models.py
@@ -83,9 +83,7 @@ class Class(models.Model):
     semester = models.CharField(max_length=25,choices=Semester)
     section = models.CharField(max_length=10,choices=Section)
     shift = models.CharField(max_length=20,choices=shift)
-    # subject = models.CharField(max_length=100)
-    # assigned_to = models.ForeignKey(Faculty, on_delete=models.CASCADE,related_name='assigned_classes')
-    coordinator = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True)
+    coordinator = models.ForeignKey(Faculty, on_delete=models.SET_NULL, null=True, unique=True)
 
     def __str__(self):
         return f"{self.course} - {self.semester} - {self.section} - {self.shift}"

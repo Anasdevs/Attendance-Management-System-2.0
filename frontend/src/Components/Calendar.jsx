4199d503e87a513c79c53c54353fc75859e81131
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-17 17:46:01 +0530

    Take Attendance Component Responsive

--- frontend/src/Components/Calendar.jsx
+++ frontend/src/Components/Calendar.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import './Calendar.css';
 export default function Calendar() {
   return (
-    <div className='rightbar'><h1 style={{textAlign:'center'}}>Coming Soon</h1>
+    <div className='example'><h1>Coming Soon</h1>
     </div>
 
   )

bf35b7bfeb6e511cbd124741cc7f923ef3cd40ff
Amit <115263300+Amitkumar71@users.noreply.github.com>
2023-08-20 18:17:52 +0530

    #Footer

--- frontend/src/Components/Calendar.jsx
+++ frontend/src/Components/Calendar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Calendar.css';
+import WithRightbarLayout from './WithRightbarLayout';
 export default function Calendar() {
   return (
+    <WithRightbarLayout>
     <div className='example'><h1>Coming Soon</h1>
     </div>
-
+    </WithRightbarLayout>
   )
 }
 
--- frontend/src/Components/WithRightbarLayout.jsx
+++ frontend/src/Components/WithRightbarLayout.jsx
@@ -9,6 +9,9 @@ const WithRightbarLayout = ({ children }) => {
       <div className="rightbar">
         {children}
       </div>
+      <footer className="footer">
+        <p> &copy; 2023 MSI | Developed by <a href="https://github.com/Amitkumar71">Amit</a> & <a href="https://github.com/Anasdevs">Anas</a> | Under <a href="https://github.com/sikhalways">Mr.Manpreet Singh</a></p>
+      </footer>
     </div>
   );
 };

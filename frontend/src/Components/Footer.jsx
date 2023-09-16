ae5f4be5c9dd30c4c5777941037b3f1ff7f066b3
Anas saifi <anassaifi1108@gmail.com>
2023-09-16 12:29:17 +0530

    Fixed footer issue

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -4,7 +4,7 @@ import './Rightbar.css';
 export default function footer() {
   return (
     <footer className="footer">
-        <p> &copy; 2023 MSI | Developed by <a href="https://github.com/Amitkumar71">Amit</a> & <a href="https://github.com/Anasdevs">Anas</a> | Under <a href="https://github.com/sikhalways">Mr.Manpreet Singh</a></p>
+        <p> &copy; 2023 MSI | Developed by <a href="https://github.com/Amitkumar71">Amit</a> & <a href="https://github.com/Anasdevs">Anas</a> | Under <a href="https://github.com/sikhalways">Mr. Manpreet Singh</a></p>
     </footer>
   )
 }
